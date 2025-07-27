e89129455fcec77d2b7f24b33e1646a904de2705
Xinyi Rong <Lizard.rar@gmail.com>
2025-07-27 14:24:25 -0700

    feat: improve get-health-status

--- app/misc/api.py
+++ app/misc/api.py
@@ -1,23 +1,28 @@
-from typing import Any
+from typing import Annotated, Any
 
+from django.http import HttpRequest
 from django.utils import timezone
 from django.views.decorators.cache import never_cache
+from ipware import get_client_ip
 from ninja import Router, Schema
 from ninja.decorators import decorate_view
-from pydantic import AwareDatetime
+from pydantic import AwareDatetime, Field
 
 router = Router(tags=["Misc"])
 
 
 class HealthStatus(Schema):
     now: AwareDatetime
+    client_ip: Annotated[str, Field(examples=["192.168.1.1", "2001:db8::1"])] | None
 
 
 @router.get("/health-status", auth=None, response=HealthStatus, summary="Ping")
 @decorate_view(never_cache)
-async def get_health_status(*_: Any) -> dict[str, Any]:
-    """Simply return current time.
-
-    Can be used to check that the server is online and can communicate in real time.
-    """
-    return {"now": timezone.now()}
+async def get_health_status(request: HttpRequest) -> dict[str, Any]:
+    """Return server status and client connection information."""
+    now = timezone.now()
+    client_ip, _ = get_client_ip(request)
+    return {
+        "now": now,
+        "client_ip": client_ip,
+    }
--- tests/misc/test_api.py
+++ tests/misc/test_api.py
@@ -3,11 +3,14 @@ from http import HTTPStatus
 from django.test import Client
 from django.urls import reverse
 
-from tests.helpers import approx_now
+from tests.helpers import any_str, approx_now
 
 
 def test_get_health_status(api_client: Client) -> None:
     response = api_client.get(reverse("api-1.0.0:get-health-status"))
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {"now": approx_now()}
+    assert response.json() == {
+        "now": approx_now(),
+        "client_ip": any_str,
+    }
     assert "no-cache" in response.headers["Cache-Control"]
