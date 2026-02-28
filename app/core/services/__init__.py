1ca14daa87982e848712a3560e30ef1145d60925
Xinyi Rong <Lizard.rar@gmail.com>
2026-02-27 17:27:18 -0800

    feat: add API key service

--- app/core/services/__init__.py
+++ app/core/services/__init__.py
@@ -1,7 +1,9 @@
 __all__ = (
+    "ApiKeyService",
     "PasswordResetService",
     "UserService",
 )
 
+from .api_key import ApiKeyService
 from .password_reset import PasswordResetService
 from .user import UserService
